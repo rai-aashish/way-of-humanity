4de6b9cb7f32c5e4e6d18d6bd2d4f044cd43b379
[Aashish Rai] <[2aashishrai@gmail.com]>
2022-07-02 21:43:37 +0545

    create meta tag

--- components/common/Container.tsx
+++ components/common/Container.tsx
@@ -13,7 +13,7 @@ const Container: React.FunctionComponent<ContainerProps> = ({
   grid = false,
 }) => {
   return (
-    <div className={`max-w-[1100px] 2xl:max-w-[1200px] 3xl:max-w-[1400px] mx-auto ${noPaddingX ? '' : 'px-4'}`}>
+    <div className={`max-w-[1100px] 2xl:max-w-[1200px] 3xl:max-w-[1400px] mx-auto ${noPaddingX ? '' : 'px-5'}`}>
       <div
         className={`${grid ? 'grid grid-cols-4 md:grid-cols-12 gap-x-gutter-md lg:gap-x-gutter-lg' : ''} ${className}`}
       >
