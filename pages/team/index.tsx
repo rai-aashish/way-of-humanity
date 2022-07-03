aca03620b83ba3c9cc9a4436d48fbee2007774c3
[Aashish Rai] <[2aashishrai@gmail.com]>
2022-07-03 21:56:15 +0545

    minor

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -6,11 +6,12 @@ import Container from 'components/common/Container';
 
 interface TeamProps {
   header: PrismicDocument;
+  footer: PrismicDocument;
 }
 
-const Team: NextPage<TeamProps> = ({ header }) => {
+const Team: NextPage<TeamProps> = ({ header, footer }) => {
   return (
-    <Layout header={header}>
+    <Layout header={header} footer={footer}>
       <Container>our team</Container>
     </Layout>
   );
@@ -21,10 +22,11 @@ export default Team;
 //fetch serverside data
 export const getStaticProps: GetStaticProps = async ({ previewData }) => {
   const client = createClient({ previewData });
-  let header;
+  let header, footer;
 
   try {
     header = await client.getSingle('header');
+    footer = await client.getSingle('footer');
   } catch (error: any) {
     if (error.message === 'No documents were returned') {
       return { notFound: true };
@@ -35,7 +37,7 @@ export const getStaticProps: GetStaticProps = async ({ previewData }) => {
 
   //re-generate the page after 60 seconds of request
   return {
-    props: { header },
+    props: { header, footer },
     revalidate: 60,
   };
 };
