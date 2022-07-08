20d045e8588db0248af9d5ab237ccd5c3eb47dd7
[Aashish Rai] <[2aashishrai@gmail.com]>
2022-07-08 09:17:28 +0545

    adds shadow on focus

--- components/common/Select.tsx
+++ components/common/Select.tsx
@@ -32,9 +32,10 @@ const Select: React.FunctionComponent<SelectProps> = ({
         </label>
       )}
       <select
+        defaultValue={options[0].relation}
         className={`w-full border border-stroke-default rounded px-4 py-3 ${
           isError ? 'border-error-base' : ''
-        } outline-none focus:bg-accent-light-blue focus:border-accent-800 hover:border-accent-600`}
+        } outline-none focus:bg-accent-light-blue focus:border-accent-800 hover:border-accent-600 focus:shadow-xl`}
         name={name}
         id={name}
         onChange={onChangeHandler}
