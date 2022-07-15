e692418c471b741b72c2ef195a5f1ac767c1565e
[Aashish Rai] <[2aashishrai@gmail.com]>
2022-07-15 17:08:19 +0545

    cleaan up

--- components/MessengerChat.tsx
+++ components/MessengerChat.tsx
@@ -4,7 +4,6 @@ import Script from 'next/script';
 interface MessengerChatProps {}
 
 const MessengerChat: React.FunctionComponent<MessengerChatProps> = (props) => {
-  const { NEXT_PUBLIC_FACEBOOK_PAGE_ID: pageId } = process.env;
   return (
     <>
       <div id="fb-root"></div>
@@ -17,7 +16,7 @@ const MessengerChat: React.FunctionComponent<MessengerChatProps> = (props) => {
           __html: `
 
         var chatbox = document.getElementById('fb-customer-chat');
-        chatbox.setAttribute("page_id", "${pageId}");
+        chatbox.setAttribute("page_id", "${process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID}");
         chatbox.setAttribute("attribution", "biz_inbox");
 
               window.fbAsyncInit = function() {
